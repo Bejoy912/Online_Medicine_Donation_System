0a6cc3e15fa2c9fde5730b8b473a84d89c6142a6
Robiul Islam <ribejoy912@gmail.com>
2022-10-04 12:59:01 +0600

    first commit

---
+++ super_admin/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(MedicineName)
+admin.site.register(MedicineType)
+admin.site.register(MedicineInfo)
+admin.site.register(GiveMedicineToPatient)
+admin.site.register(Newsletter)
---
+++ user_profile/urls.py
@@ -0,0 +1,53 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', base, name='base'),
+    path('login/', login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('donor_registration/', donor_registration, name='donor_registration'),
+    path('doctor/list/', doctor_list, name='doctor_list'),
+    path('about/', about, name='about'),
+    path('service/', service, name='service'),
+    path('contact/', contact, name='contact'),
+    path('profile/', profile, name='user_profile'),
+    path('update_profile/', update_profile, name='update_profile'),
+    path('user_password_change/', user_password_change, name='user_password_change'),
+
+
+
+    # donor
+    path('create_donor_give_medicine_info/', create_donor_give_medicine_info, name='create_donor_give_medicine_info'),
+    path('list_donor_give_medicine_info/', list_donor_give_medicine_info, name='list_donor_give_medicine_info'),
+
+
+    path('create_make_appointment/', create_make_appointment, name='create_make_appointment'),
+
+
+    # doctor
+    path('doctor_list_make_appointment_info/', doctor_list_make_appointment_info,
+         name='doctor_list_make_appointment_info'),
+    path('doctor_give_prescription/<int:patient_id>/', doctor_give_prescription, name='doctor_give_prescription'),
+    path('doctor_is_check_status_change/<int:patient_id>/', doctor_is_check_status_change,
+         name='doctor_is_check_status_change'),
+    path('doctor_view_prescription/<int:patient_id>/', doctor_view_prescription,
+         name='doctor_view_prescription'),
+    path('doctor_update_prescription/<int:patient_id>/', doctor_update_prescription,
+         name='doctor_update_prescription'),
+
+    path('patient_list_make_appointment_info', patient_list_make_appointment_info,
+         name='patient_list_make_appointment_info'),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='restore_password.html'),
+         name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='restore_password_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='restore_password_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
+
+]
